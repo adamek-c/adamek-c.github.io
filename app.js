584863a1ff8e87b2d0d617c19375d499c26e0885
Adam Ciołek <adasciolek95@gmail.com>
2022-06-24 15:42:20 +0200

    show and hide navbar when is scroll

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const hamburger = document.querySelector(".navbar__menu");
 const navbarMobile = document.querySelector(".navbar__mobile");
 const linkNavbar = document.querySelectorAll(".menu__link__mobile");
-
-const navbar = document.querySelector(".navbar");
-
-// const boxSkills = document.querySelectorAll(".skill__boxes__container");
-// const skills = document.querySelectorAll(".skill__boxes__list");
-// const skillInfoTech = document.querySelector(".skill__info__tech");
-// const skillTitle = document.querySelector(".skill__info__title");
+const navbar = document.querySelector("#navbar");
 
 hamburger.addEventListener("click", () => {
 	navbarMobile.classList.toggle("active-mobile");
@@ -19,31 +13,18 @@ linkNavbar.forEach((link) => {
 	);
 });
 
-// for mobile devices
-// boxSkills.forEach((box) => {
-// 	box.addEventListener("click", () => {
-// 		const boxId = box.getAttribute("data-set");
-// 		skills.forEach((skill) => {
-// 			const skillId = skill.getAttribute("data-number");
-// 			if (boxId === skillId) {
-// 				skill.classList.toggle("active");
-// 			}
-// 		});
-// 	});
-// });
-
-// for desktop devices
-// boxSkills.forEach((box) => {
-// 	box.addEventListener("click", () => {
-// 		const boxId = box.getAttribute("data-set");
-// 		const boxText = box.lastElementChild.textContent;
-// 		skillTitle.innerHTML = boxText;
-// 		skills.forEach((skill) => {
-// 			const skillId = skill.getAttribute("data-number");
-// 			if (boxId === skillId) {
-// 				const skillText = skill.innerHTML;
-// 				skillInfoTech.innerHTML = skillText;
-// 			}
-// 		});
-// 	});
-// });
+document.addEventListener("scroll", () => {
+	let currentScrollPos = window.pageYOffset;
+	if (scroll > currentScrollPos && currentScrollPos !== 0) {
+		navbar.style.top = "0";
+		navbar.style.backgroundColor = "white";
+	} else if (currentScrollPos === 0) {
+		navbar.style.backgroundColor = "transparent";
+	} else {
+		navbar.style.top = "-150px";
+		if (navbarMobile.classList.contains("active-mobile")) {
+			navbarMobile.classList.remove("active-mobile");
+		}
+	}
+	scroll = currentScrollPos;
+});
