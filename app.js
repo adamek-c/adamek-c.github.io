5c113f16baf70d044f0fa2c3394b5df515761b53
Adam Ciołek <adasciolek95@gmail.com>
2022-06-24 10:17:58 +0200

    change section skills

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const linkNavbar = document.querySelectorAll(".menu__link__mobile");
 
 const navbar = document.querySelector(".navbar");
 
-const boxSkills = document.querySelectorAll(".skill__boxes__container");
-const skills = document.querySelectorAll(".skill__boxes__list");
-const skillInfoTech = document.querySelector(".skill__info__tech");
-const skillTitle = document.querySelector(".skill__info__title");
+// const boxSkills = document.querySelectorAll(".skill__boxes__container");
+// const skills = document.querySelectorAll(".skill__boxes__list");
+// const skillInfoTech = document.querySelector(".skill__info__tech");
+// const skillTitle = document.querySelector(".skill__info__title");
 
 hamburger.addEventListener("click", () => {
 	navbarMobile.classList.toggle("active-mobile");
@@ -20,30 +20,30 @@ linkNavbar.forEach((link) => {
 });
 
 // for mobile devices
-boxSkills.forEach((box) => {
-	box.addEventListener("click", () => {
-		const boxId = box.getAttribute("data-set");
-		skills.forEach((skill) => {
-			const skillId = skill.getAttribute("data-number");
-			if (boxId === skillId) {
-				skill.classList.toggle("active");
-			}
-		});
-	});
-});
+// boxSkills.forEach((box) => {
+// 	box.addEventListener("click", () => {
+// 		const boxId = box.getAttribute("data-set");
+// 		skills.forEach((skill) => {
+// 			const skillId = skill.getAttribute("data-number");
+// 			if (boxId === skillId) {
+// 				skill.classList.toggle("active");
+// 			}
+// 		});
+// 	});
+// });
 
 // for desktop devices
-boxSkills.forEach((box) => {
-	box.addEventListener("click", () => {
-		const boxId = box.getAttribute("data-set");
-		const boxText = box.lastElementChild.textContent;
-		skillTitle.innerHTML = boxText;
-		skills.forEach((skill) => {
-			const skillId = skill.getAttribute("data-number");
-			if (boxId === skillId) {
-				const skillText = skill.innerHTML;
-				skillInfoTech.innerHTML = skillText;
-			}
-		});
-	});
-});
+// boxSkills.forEach((box) => {
+// 	box.addEventListener("click", () => {
+// 		const boxId = box.getAttribute("data-set");
+// 		const boxText = box.lastElementChild.textContent;
+// 		skillTitle.innerHTML = boxText;
+// 		skills.forEach((skill) => {
+// 			const skillId = skill.getAttribute("data-number");
+// 			if (boxId === skillId) {
+// 				const skillText = skill.innerHTML;
+// 				skillInfoTech.innerHTML = skillText;
+// 			}
+// 		});
+// 	});
+// });
